637372b0e2e9e1e614ab44be10313ea3d0209188
PeIndo <123163530+PeIndo@users.noreply.github.com>
2023-01-21 15:46:35 +0100

    Update streamlit_app.py
    stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,6 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?','Kiwi')
 fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
-# streamlit.text(fruityvice_response2.json())
-# format json response 
-fruityvice_normalized2 = pandas.json_normalize(fruityvice_response2.json().name)
+fruityvice_normalized2 = pandas.json_normalize(fruityvice_response2.json())
 # print data int tablesheet
-streamlit.write('Thanks for adding ', fruityvice_normalized2)
+streamlit.write('Thanks for adding ', add_my_fruit)
