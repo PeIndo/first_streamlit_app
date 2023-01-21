eb7d47b431da78cf4710c3eaea0d4680b8146397
PeIndo <123163530+PeIndo@users.noreply.github.com>
2023-01-21 16:38:23 +0100

    Update streamlit_app.py
    variable for add
    close connections
    change header

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ try:
 except URLError as e:
     streamlit.error()
     
-streamlit.header("The fruit load list contains :")
+streamlit.header("View our Fruit List - Add your favorites!")
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
@@ -48,17 +48,19 @@ def get_fruit_load_list():
 if streamlit.button("Get Fruit Load List"):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_row = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_row)
 
 #streamlit.stop()
 def insert_row_snowflake(new_fruit):
      with my_cnx.cursor() as my_cur:
-       my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+       my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit + "')")
        return "Thanks for adding " + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
 if streamlit.button("Add a fruit to the list"):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       back_from_adding = insert_row_snowflake(add_my_fruit)
+      my_cnx.close()
       streamlit.write(back_from_adding)
 
